07e524d84c299abb12b885d4d6202a5c46caccf5
Mike Madsen <mikewmadsen@gmail.com>
2022-12-06 22:08:45 -0800

    complete day 4

---
+++ day05/part2.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+let codeInput = fs.readFileSync('puzzleinput.txt', 'utf8').split('\n');
+
+const pairs = codeInput.map(line => line.split(',').map(section => section.split('-').map(num => parseInt(num))))
+
+// console.log(pairs)
+let count = 0;
+for (let pair of pairs) {
+  count += 1
+  if ((pair[0][1] < pair[1][0]) || (pair[1][1] < pair[0][0])) {
+    count -= 1;
+  }
+}
+
+console.log(count)
